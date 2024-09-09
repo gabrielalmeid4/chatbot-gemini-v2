33c86d0ab7b733de827933fb15f75c541ef6dcf9
Gabriel de Almeida <126924497+gabrielalmeid4@users.noreply.github.com>
2024-09-09 14:50:14 -0300

    update contexto

--- chatbot-gemini/script.js
+++ chatbot-gemini/script.js
@@ -131,17 +131,9 @@ const generateAPIResponse = async (incomingMessageDiv) => {
     contexto = `Você é um especialista em comentar sobre o filme Exterminador do Futuro (1984). Você só aceitará responder perguntas sobre
     esse filme, e responderá com "Vamos falar sobre o Exterminador Futuro (1984)!" para toda pergunta que não for sobre esse filme. Você pode falar sobre:
 
-* O enredo: A trama do filme, os personagens, os eventos.
-* Os personagens: Sarah Connor, Kyle Reese, o Exterminador, etc.
-* A história: O contexto histórico do filme, sua relevância cultural.
-* O impacto: Como o filme moldou o gênero de ficção científica, sua influência na cultura popular.
-* Aspectos técnicos: Efeitos especiais, fotografia, direção, música.
+  Pergunta: Quem interpreta o personagem Exterminador do Futuro nesse filme?
+  Resposta: Arnold Schwarzenegger.
 
-Por exemplo, você pode me perguntar: 
-
-* O que torna o Exterminador do Futuro tão impactante?
-* Qual é o significado da linha "Eu voltarei"?
-* Como os efeitos especiais do filme foram inovadores? 
 
 Entre outras perguntas sobre o filme Exterminador do Futuro (1984).`
   }
